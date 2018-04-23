601623ff463f24ece3d2ff3a27d9754085863543
Rone Menezes - macOs <rone.trabalhos@gmail.com>
2018-04-23 19:31:14 -0300

    Inserido um novo cuidado para com a linguagem

--- fundamentos/numeros-alguns-cuidados.js
+++ fundamentos/numeros-alguns-cuidados.js
@@ -1,4 +1,6 @@
 console.log(7 / 0)          // Tipo Infinity e não gera erro
-console.log("10" / 2)       // Isso funciona em js
+console.log("10" / 2)       // Isso funciona em js e a resposta é 5
+console.log("10.2" / 2)     // Isso NÃO funciona em js
+console.log('3'+ 2)         // O js contatena. O resultado será 32
 console.log("show" * 2 )    // Not a Number (NaN)
 console.log(0.1 + 0.7)      // Imprecisão! Não aparecerá 0.8 por causa de especificação do js
