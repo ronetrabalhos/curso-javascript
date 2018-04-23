a3b0463d247d924a9b2af3df497baf01d33fae17
Rone Menezes - macOs <rone.trabalhos@gmail.com>
2018-04-23 18:56:40 -0300

    Aula 16: Number - alguns cuidados

---
+++ fundamentos/numeros-alguns-cuidados.js
@@ -0,0 +1,4 @@
+console.log(7 / 0)          // Tipo Infinity e não gera erro
+console.log("10" / 2)       // Isso funciona em js
+console.log("show" * 2 )    // Not a Number (NaN)
+console.log(0.1 + 0.7)      // Imprecisão! Não aparecerá 0.8 por causa de especificação do js
