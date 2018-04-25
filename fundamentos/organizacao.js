6f34e9c834aa197cde724d4d6d9bae7404a253b7
ronetrabalhos <rone.trabalhos@gmail.com>
2018-04-25 12:20:52 -0300

    Teste commit usando Git Desktop
    Utilizei outro programa para ver se ocorrerá erro

--- fundamentos/organizacao.js
+++ fundamentos/organizacao.js
@@ -1,4 +1,5 @@
 /* nenhum código */
 /* este comentário foi inserido no notebook Dell. Commit via sourcetree */
 console.log('teste code runner')
-console.log('teste de ' + 'concatenação')
+console.log('teste de ' + 'concatenação 01')
+console.log('teste de ' + 'concatenação' + '02')
