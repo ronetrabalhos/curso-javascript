61b6f954b3e5821fb04c83b86e1cf673a0b6861f
Rone Menezes - macOs <rone.trabalhos@gmail.com>
2018-04-28 17:29:51 -0300

    043 - Operadores: Aritméticos

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,37 @@
+/*
+ * NOTAS INTERESSANTES
+ * 
+ * ----------------------------
+ * OPERAÇÕES BINÁRIAS 
+ * ----------------------------
+ * operando / operador
+ * a++  (post-fixed)
+ * 
+ * operador / operando
+ * ++a  (pre-fixed)
+ * 
+ * operando / operador / operando
+ * a + b (in-fixed)
+ * 
+ * ----------------------------
+ * OPERAÇÃO UNÁRIA 
+ * ----------------------------
+ * -a
+*/
+
+
+const [a, b, c, d] = [3, 5, 1, 15]  // variáveis criadas com OD
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 // par = 0, impar = 1
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+
+
+
+
+
+
