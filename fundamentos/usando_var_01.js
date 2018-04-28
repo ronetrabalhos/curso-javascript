f8d7e8e4b65dc9712a265f68d63fb7a8a093f2e4
Rone Menezes - macOs <rone.trabalhos@gmail.com>
2018-04-28 12:08:04 -0300

    Aula 27 - Declaração de variáveis com var #01

--- fundamentos/usando_var_01.js
+++ fundamentos/usando_var_01.js
@@ -1,3 +1,12 @@
+/* 
+ * a variável var só possui 2 escopos:
+ * 
+ * - global: visível em toda a aplicação
+ * - escopo de função: visível apenas dentro da função
+*/
+
+
+
 // Dentro de um bloco de código, não sendo uma função
 // a variável var será visivel globalmente
 {
@@ -9,9 +18,18 @@
         }
     }
 }
-console.log(sera)
+console.log('solto: ' + sera)
 
 function teste(){
     var local = 123
-    
+    console.log('valor local: ' + local);
+    console.log('dentro da function teste: ' + sera);
 }
+
+// como a variavel local foi criada dentro de uma função,
+// a funçao delimita o seu escopo de visibilidade
+// isso causará erro de execução
+// console.log(local);
+
+// chamando a funcao
+teste()
