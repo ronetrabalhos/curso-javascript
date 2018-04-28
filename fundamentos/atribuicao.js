8ca62dafc4101dfd11738081fbae02308c81b708
Rone Menezes - macOs <rone.trabalhos@gmail.com>
2018-04-28 15:11:34 -0300

    038 - Operadores: Atribuição

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,31 @@
+/*
+ * O sinal de igual (=) é o sinal de atribuição
+ * 
+*/
+
+const a = 7
+let b = 3
+
+// Atribuições com atalhos
+
+// Atribuição aditiva 
+// Junto com a atribuição é feito uma adição
+
+b += a // b = b + a
+console.log(b)
+
+// atribuição subtrativa
+b -= 4 // b = b - 4
+console.log(b)
+
+// atribuição multiplicativa
+b *= 2 // b = b * 2
+console.log(b)
+
+// atribuição divisiva
+b /= 2 // b = b / 2
+console.log(b)
+
+// atribuição modular
+b %= 2 // b = b % 2   // par = 0, impar = 1
+console.log(b)
