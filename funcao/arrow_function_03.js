43d6e5db410a1ab243f813fc72d5d823e9f8aff2
Rone Menezes - macOs <rone.trabalhos@gmail.com>
2018-04-29 17:39:46 -0300

    070 - Funções Arrow #03

---
+++ funcao/arrow_function_03.js
@@ -0,0 +1,24 @@
+/*
+ * 
+*/
+
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+// global é um objeto
+// global no console é window
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
