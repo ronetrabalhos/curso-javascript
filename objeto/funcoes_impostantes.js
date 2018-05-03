21580950a737faaf5df60c4d78f7a8047e0ffd7a
ronemenezes <rone.trabalhos@gmail.com>
2018-05-03 16:29:52 -0300

    091 - Funções importantes de Objeto

---
+++ objeto/funcoes_impostantes.js
@@ -0,0 +1,9 @@
+
+
+// Object.keys: retorna as chaves de um objeto
+// Object.values: retorna os valores de um determinado objeto
+// Object.entries: retorna um array de arrays com todos os valores
+// Object.defineProperty: definir uma propriedade de um objeto 
+// Object.assign: concatenação de objetos, gerando um novo objeto. Caso ocorra de um algum
+//                objeto ter o mesmo atributo de outro já inserido, o valor dessa propriedade
+//                será atualizado.
