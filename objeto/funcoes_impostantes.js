9dc9df729568ea56665b5873b036daefeaef52da
Rone Menezes - macOs <rone.trabalhos@gmail.com>
2018-05-05 11:16:31 -0300

    091 - Funções Importantes de Objeto (Anotações adicionais e código)

--- objeto/funcoes_impostantes.js
+++ objeto/funcoes_impostantes.js
@@ -7,3 +7,37 @@
 // Object.assign: concatenação de objetos, gerando um novo objeto. Caso ocorra de um algum
 //                objeto ter o mesmo atributo de outro já inserido, o valor dessa propriedade
 //                será atualizado.
+
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`)
+})
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true,   // será enumerado na lista de chaves do array?
+    writable: false,    // pode ser modificado?
+    value: '01/01/2019' // valor
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
+
+// Object.assign (ECMAScript 2015)
+const dest = { a: 1 }
+const o1 = { b: 2 }
+const o2 = { c: 3, a: 4 }
+const obj = Object.assign(dest, o1, o2)
+
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
